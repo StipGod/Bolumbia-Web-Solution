a089b386e6019053770f8e2151f0be6d6a3eeb6f
StipGod <72163911+StipGod@users.noreply.github.com>
2023-11-17 14:53:47 -0500

    + formRoutes.js

--- census-form-api/src/index.js
+++ census-form-api/src/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const authRoutes = require('./routes/authRoutes');
+const formRoutes = require('./routes/formRoutes');
 
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
+app.use('/api/form', formRoutes);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Census Form API!');
---
+++ census-form-api/src/routes/formRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const formController = require('../controllers/formController');
+
+// el user id seria la cedula esa o cualquie cosa
+router.get('/:userId', formController.getForm);
+
+router.post('/save', formController.saveForm);
+
+router.post('/submit', formController.submitForm);
+
+module.exports = router;
